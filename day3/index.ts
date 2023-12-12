a3ca53ebc7cd4668214bcf46daf703abce41151b
Michael Abrahamian <mabrahamian@atlassian.com>
2023-12-12 17:00:52 +1100

    feat(day3): add day 3 part 2

--- day3/index.ts
+++ day3/index.ts
@@ -138,3 +138,82 @@ const getSumOfEnginePartNumbers = (): number => {
 };
 
 console.log('Sum:', getSumOfEnginePartNumbers());
+
+// Part 2
+// find all the "gears" (* symbols) that are adjacent to exactly two part numbers
+// multiply part numbers
+// sum up
+
+const getSumOfGearRatios = (engineSchematic: EngineSchematic): number =>
+    Object.values(engineSchematic)
+        .map((schematicForLine, lineIndex) => {
+            const { parts, symbols } = schematicForLine;
+
+            const gearSymbols = symbols.filter(
+                (symbol) => symbol.value === '*'
+            );
+
+            const getPartsAdjacentToSymbol = (
+                gearSymbol: SchematicSymbol
+            ): PartNumber[] => {
+                const isPartToTheLeft = (part: PartNumber): boolean =>
+                    part.endIndex === gearSymbol.index - 1;
+                const isPartToTheRight = (part: PartNumber): boolean =>
+                    part.startIndex === gearSymbol.index + 1;
+
+                const partToTheLeft = parts.find(isPartToTheLeft);
+                const partToTheRight = parts.find(isPartToTheRight);
+
+                const partAdjacentOnLineAbove = engineSchematic[
+                    lineIndex - 1
+                ]?.parts.find(
+                    (part) =>
+                        gearSymbol.index >= part.startIndex - 1 &&
+                        gearSymbol.index <= part.endIndex + 1
+                );
+
+                const partAdjacentOnLineBelow = engineSchematic[
+                    lineIndex + 1
+                ]?.parts.find(
+                    (part) =>
+                        gearSymbol.index >= part.startIndex - 1 &&
+                        gearSymbol.index <= part.endIndex + 1
+                );
+
+                const allPartsAdjacentToSymbol = [
+                    partToTheLeft,
+                    partToTheRight,
+                    partAdjacentOnLineAbove,
+                    partAdjacentOnLineBelow,
+                ].filter((part): part is PartNumber => Boolean(part));
+
+                return allPartsAdjacentToSymbol;
+            };
+
+            const sumOfPartNumbersAdjacentToSymbols = gearSymbols
+                .map((gearSymbol) => {
+                    const partsAdjacentToSymbol =
+                        getPartsAdjacentToSymbol(gearSymbol);
+
+                    console.log('partsAdjacentToSymbol', partsAdjacentToSymbol);
+
+                    return partsAdjacentToSymbol.length === 2
+                        ? partsAdjacentToSymbol.reduce(
+                              (acc, part) => part.value * acc,
+                              1
+                          )
+                        : undefined;
+                })
+                .filter((value): value is number => value !== undefined);
+
+            return sumOfPartNumbersAdjacentToSymbols;
+        })
+        .flat()
+        .reduce((acc, gearRatios) => {
+            return acc + gearRatios;
+        }, 0);
+
+console.log(
+    'Part 2',
+    getSumOfGearRatios(createEngineSchematicFromInputFile(INPUT_FILE_PATH))
+);
